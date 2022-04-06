ce87dec60af49526ddaa7270d09eda72c2660809
Kate Folk <folkenrothk@allegheny.edu>
2022-04-06 14:29:19 -0400

    End of Class: random btn & verify

--- docs/script/ui.js
+++ docs/script/ui.js
@@ -1,16 +1,15 @@
 // Time to have fun
 
 // Notes: proving we can do style in java (this is read last)
-document.getElementById("top-left").style.borderRadius = "0 20% 0 20%"
-document.getElementById("top-right").style.borderRadius = "20% 0 20% 0";
-document.getElementById("bottom-left").style.borderRadius = "20% 0 20% 0";
-document.getElementById("bottom-right").style.borderRadius = "0 20% 0 20%";
-
+    document.getElementById("top-left").style.borderRadius = "0 20% 0 20%"
+    document.getElementById("top-right").style.borderRadius = "20% 0 20% 0";
+    document.getElementById("bottom-left").style.borderRadius = "20% 0 20% 0";
+    document.getElementById("bottom-right").style.borderRadius = "0 20% 0 20%";
 
 // Variable Declarations
 var startBtn = document.getElementById("start-button");
 
-var gameBtn = document.querySelectorAll(".game-button");
+var gameBtns = document.querySelectorAll(".game-button");
 
 var tLeft = document.getElementById("top-left");
 var tRight = document.getElementById("top-right");
@@ -18,27 +17,35 @@ var bLeft = document.getElementById("bottom-left");
 var bRight = document.getElementById("bottom-right");
 
 var userChoices = [];
-
 var simonSays = [];
-//Array.from({length:20}, ())
 
+//Modules of Game Pieces
+//Making Levels & Turns
 
-//Start Button
-startBtn.addEventListener("click", () => {
-    document.getElementById("points").innerText = "Points: 0"; // set points back to zero
-    startBtn.style.color = "#222222";
-    userChoices = []; // restart user choice sequence
+    // put game-button ids in an array
+    const elements = Array.from(gameBtns).map(btn => {
+        return btn.id //tell me what was pressed (outcome of ids)
+    });
 
-    // set all buttons to un-highlighted
-    tLeft.style.opacity = "50%";
-    tRight.style.opacity = "50%";
-    bLeft.style.opacity = "50%";
-    bRight.style.opacity = "50%";
-});
+    // Function to choose random button
+    const chooseRandomBtn = () => {
+        let choice = Math.floor(Math.random() * elements.length);
+        return elements[choice];
+    };
+
+    // Check user choices vs game sequence
+    const verifyPattern = () => {
+        if(userChoices.length !== simonSays.length) return false;
+        for(var i = 0; i < patter.length; i++)
+            {
+                if(pattern[i] !== choice[i]) return false;
+            }
+            return true;
+    };
+
 
 // Game Buttons
-
-gameBtn.forEach(elem =>{
+gameBtns.forEach(elem =>{
     elem.addEventListener("click", (e) => {
         let source = e.target.id; //displaying button id in console
         userChoices.push(source); //collecting user button clicks
@@ -49,19 +56,34 @@ gameBtn.forEach(elem =>{
     });
 });
 
-tLeft.addEventListener("click", () => {
-    console.log("Green bb");
+    tLeft.addEventListener("click", () => {
+        console.log("Green bb");
+    });
+    tRight.addEventListener("click", () => {
+        console.log("BLOOD RED... jk");
+    });
+    bLeft.addEventListener("click", () => {
+        console.log("Yellowy sunshiny day");
+        
+    });
+    bRight.addEventListener("click", () => {
+        console.log("I'm blue ba da de da da dum");
+    });
+
+//Start Button
+startBtn.addEventListener("click", () => {
+    document.getElementById("points").innerText = "Points: 0"; // set points back to zero
+    startBtn.style.color = "#222222";
+    userChoices = []; // restart user choice sequence
+    simonSays = []; // restart game pattern
+
+    // set all buttons to un-highlighted
+    tLeft.style.opacity = "50%";
+    tRight.style.opacity = "50%";
+    bLeft.style.opacity = "50%";
+    bRight.style.opacity = "50%";
 });
 
-tRight.addEventListener("click", () => {
-    console.log("BLOOD RED... jk");
-});
+// Game Functionality
 
-bLeft.addEventListener("click", () => {
-    console.log("Yellowy sunshiny day");
-    
-});
-
-bRight.addEventListener("click", () => {
-    console.log("I'm blue ba da de da da dum");
-});
+simonSays.push(chooseRandomBtn());
