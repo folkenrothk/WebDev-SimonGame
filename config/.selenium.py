0ff9fa4e552f98d82856516b3c3cc8a1fc7fd713
dluman <dluman@allegheny.edu>
2022-04-08 19:50:45 -0400

    Kate passes the run test now

--- config/.selenium.py
+++ config/.selenium.py
@@ -16,8 +16,8 @@ def main():
   # Run in headless mode
 
   options = webdriver.ChromeOptions()
-  options.add_argument("--headless")
-  options.headless = True
+  #options.add_argument("--headless")
+  #options.headless = True
 
   service = Service(ChromeDriverManager().install())
   driver = webdriver.Chrome(options=options, service=service)
@@ -25,13 +25,13 @@ def main():
 
   page = sys.argv[1]
 
-  pages_api = os.getenv("PAGES_URL")
-  data = json.loads(pages_api)
-  endpoint = data["html_url"]
+  #pages_api = os.getenv("PAGES_URL")
+  #data = json.loads(pages_api)
+  #endpoint = data["html_url"]
 
   # Local Testing
-  #driver.get(f"https://allegheny-computerscience-302-s2022.github.io/cmpsc-302-week-4-basic-javascript-solution/{page}")
-  driver.get(f"{endpoint}{page}")
+  driver.get(f"https://allegheny-computerscience-302-s2022.github.io/cmpsc-302-week-4-basic-javascript-folkenrothk/{page}")
+  #driver.get(f"{endpoint}{page}")
 
   ids = ["points", "start"]
 
@@ -42,14 +42,20 @@ def main():
   for id in ids:
     for element in elements:
       id_attr = element.get_attribute("id")
-      if id in id_attr: discovered[id] = element
+      if id in id_attr:
+        discovered[id] = element
+
+  sleep(2)
 
   discovered["start"].click()
   if not discovered["points"].text.lower() == "points: 0": exit(1)
 
   sleep(2)
 
-  if not len(driver.execute_script("""return gamePattern""")) > 0: exit(1)
+  try:
+    if not len(driver.execute_script("""return gamePattern""")) > 0: exit(1)
+  except:
+    if not len(driver.execute_script("""return simonSays""")) > 0: exit(1)
 
   print("SUCCESS!")
 
